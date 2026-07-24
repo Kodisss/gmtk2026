36129a1b1e5d0f3a59a17e683ae4e6012e5c9458
Ash <afalzon12@gmail.com>
2026-07-24 12:59:17 +0200

    fixed the camera

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float offset;
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform target;
 
-    // Start is called before the first frame update
-    void Start()
+    public void SetOffset(float newOffset)
     {
-        
+        offset = newOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = target.position + offset;  
+        Vector3 targetPosition = target.position + new Vector3(offset, 0f, -10f);  
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
