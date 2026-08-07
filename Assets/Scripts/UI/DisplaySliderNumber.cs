ee9290aa0a723cbd9139beee98e7186a5526c2d5
Ash <afalzon12@gmail.com>
2026-08-07 22:59:55 +0200

    actually implemented settings in the menu

---
+++ Assets/Scripts/UI/DisplaySliderNumber.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class DisplaySliderNumber : MonoBehaviour
+{
+    private TMP_Text numberDisplay;
+    private Slider numberSlider;
+
+    private void Start()
+    {
+        numberSlider = GetComponent<Slider>();
+        numberDisplay = GetComponentInChildren<TMP_Text>();
+    }
+
+    public void ChangeNumber()
+    {
+        numberDisplay.text = numberSlider.value.ToString("F2");
+    }
+}
--- Assets/Scripts/UI/TItleScreenManager.cs
+++ Assets/Scripts/UI/TItleScreenManager.cs
@@ -3,13 +3,48 @@ using UnityEngine.SceneManagement;
 
 public class TItleScreenManager : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingScreen;
+
+    private GameSettings gameSettings;
+
+    private void Start()
+    {
+        gameSettings = GameSettings.Instance;
+    }
+
     public void StartTheGame()
     {
         SceneManager.LoadScene("Game");
     }
 
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        settingScreen.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        mainMenu.SetActive(true);
+        settingScreen.SetActive(false);
+    }
+
     public void QuitTheGame()
     {
         Application.Quit();
+    }
+
+    public void ModifySFXVolume(float value)
+    {
+        gameSettings.SoundVolume = value;
+    }
+    public void ModifyMusicVolume(float value)
+    {
+        gameSettings.MusicVolume = value;
+    }
+    public void ModifyVoiceVolume(float value)
+    {
+        gameSettings.VoiceVolume = value;
     }
 }
